0f079923de595ffb543f6becf277be5decbb7c15
Rafael <rafaelv.boscato@hotmail.com>
2025-07-15 15:01:26 -0300

    enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
+  app.enableCors({
+    origin: 'https://www.manager-sportings.com.br',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
